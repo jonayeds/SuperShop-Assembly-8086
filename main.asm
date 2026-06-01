cf3e215739cf9a3ccde1bec67ec050a3aa18da4b
jonayeds <jonayeeds@gmail.com>
2026-06-01 13:12:21 +0600

    handled invalid character input

--- main.asm
+++ main.asm
@@ -15,91 +15,101 @@
 
 .code
 next_line proc
-                      MOV  AH, 02h
-                      MOV  DL, 10
-                      INT  21h
-                      ret
+                            MOV  AH, 02h
+                            MOV  DL, 10
+                            INT  21h
+                            ret
 next_line endp
 
 print_string proc
-                      MOV  dx, si
-                      mov  ah, 09h
-                      int  21h
-                      ret
+                            MOV  dx, si
+                            mov  ah, 09h
+                            int  21h
+                            ret
 print_string endp
 
-display_list proc                                         ; Required: list offset in the DI register && each element = 16 bit && CX= Number of elements
-                      mov  bl, 1
+display_list proc                                               ; Required: list offset in the DI register && each element = 16 bit && CX= Number of elements
+                            mov  bl, 1
      l1:
-                      ; print number and separator
-                      mov  dl, bl
-                      add  dl, '0'
-                      mov  ah, 02h
-                      int  21h
-                      mov  dl, '.'
-                      mov  ah, 02h
-                      int  21h
-                      mov  dl, ' '
-                      mov  ah, 02h
-                      int  21h
+                            ; print number and separator
+                            mov  dl, bl
+                            add  dl, '0'
+                            mov  ah, 02h
+                            int  21h
+                            mov  dl, '.'
+                            mov  ah, 02h
+                            int  21h
+                            mov  dl, ' '
+                            mov  ah, 02h
+                            int  21h
 
-                      ; print category string
-                      mov  si, [di]
-                      call print_string
-                      call next_line
+                            ; print category string
+                            mov  si, [di]
+                            call print_string
+                            call next_line
 
-                      add  di, 2
-                      inc  bl
-                      loop l1
-                      ret
-                      endp display_list
+                            add  di, 2
+                            inc  bl
+                            loop l1
+                            ret
+                            endp display_list
 
 main proc
-                      mov  AX, @data
-                      mov  ds, ax
-                      MOV  si, OFFSET intro
-                      call print_string
-                      call next_line
-                      mov  si, offset instructions
-                      call print_string
-                      call next_line
-                      ; Display the categories numbered
-                      mov  di, OFFSET catPtrs
-                      mov  cl, [num_of_category]
-                      call display_list
+                            mov  AX, @data
+                            mov  ds, ax
+                            MOV  si, OFFSET intro
+                            call print_string
+                            call next_line
+                            mov  si, offset instructions
+                            call print_string
+                            call next_line
+                            ; Display the categories numbered
+                            mov  di, OFFSET catPtrs
+                            mov  cl, [num_of_category]
+                            call display_list
 
      ; take input
      select_category_input:
-                      mov  si, OFFSET select_category
-                      call print_string
-                      mov  cx,3
-                      mov  dl,0
-                      mov  al,0
-                      mov  bl,10
+                            mov  si, OFFSET select_category
+                            call print_string
+                            mov  cx,3
+                            mov  dl,0
+                            mov  al,0
+                            mov  bl,10
      l2:
-                      mov  ah, 01h
-                      int  21h
-                      cmp  al, 0Dh                        ; if pressed enter, then exit input
-                      je   exit_input
-                      mov bh, al
-                      sub bh, 30h
-                      mov al, dl
-                      mul bl
-                      add  dl, bh                         ; input stored in dl as Hex
-                      LOOP l2
+                            mov  ah, 01h
+                            int  21h
+                            cmp  al, 0Dh                        ; if pressed enter, then exit input
+                            je   exit_input
+                            cmp  al, '0'
+                            jl   invalid_character
+                            cmp  al, '9'
+                            jg   invalid_character
+                            mov  bh, al
+                            sub  bh, 30h
+                            mov  al, dl
+                            mul  bl
+                            add  dl, bh                         ; input stored in dl as Hex
+                            LOOP l2
+                            jmp  exit_input
+     invalid_character:                                    ; Restart input if the caracter is invalid
+                            mov  si, OFFSET invalid_input
+                            call print_string
+                            call next_line
+                            jmp  select_category_input
      exit_input:
-                    mov bl, [num_of_category]
-                      cmp  dl, bl
-                      jle   valid_category
-                      mov  si, OFFSET invalid_input
-                      call print_string
-                      call next_line
-                      jmp select_category_input
+                            mov  bl, [num_of_category]
+                            cmp  dl, bl
+                            jle  valid_category
+                            mov  si, OFFSET invalid_input
+                            call print_string
+                            call next_line
+                            jmp  select_category_input
 
      valid_category:
 
      ex:
-                      MOV  AH, 4Ch
-                      INT  21h
+                            MOV  AH, 4Ch
+                            INT  21h
 main endp
 end main
